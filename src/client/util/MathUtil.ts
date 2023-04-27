bb766eb8bd49fdc3bb8a3625b108c03976819cc9
Antony Garand <9059487+AntonyGarand@users.noreply.github.com>
2023-04-26 23:16:50 -0400

    POC: Working camera "rails"

--- src/client/util/MathUtil.ts
+++ src/client/util/MathUtil.ts
@@ -1,2 +1,11 @@
 export const clamp = (num: number, min: number, max: number) =>
     Math.min(Math.max(num, min), max);
+
+export const lerp = (start: number, end: number, progress: number) => {
+    if(progress <= 0){
+        return start;
+    } else if(progress >= 1){
+        return end;
+    }
+    return start + (end - start) * progress;
+}
