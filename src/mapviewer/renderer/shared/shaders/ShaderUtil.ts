d5e83af21799f2d98057c739a64edc70870ddc1d
dennisdev <me@dennis.dev>
2023-12-11 19:11:34 +0100

    Fix shader defines

--- src/mapviewer/renderer/shared/shaders/ShaderUtil.ts
+++ src/mapviewer/renderer/shared/shaders/ShaderUtil.ts
@@ -1,11 +1,11 @@
 export type ProgramSource = [string, string];
 
 export function prependDefines(source: string, defines: string[]): string {
-    const header = "#version 300 es\n";
+    const header = "#version 300 es";
 
     const newHeader = defines.reduce((acc, define) => {
         return acc + "#define " + define + " 1\n";
-    }, header);
+    }, header + "\n");
 
     return source.replace(header, newHeader);
 }
