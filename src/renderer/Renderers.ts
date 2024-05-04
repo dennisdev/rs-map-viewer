f5e8d13ea8ade229eb566cf3db3e233dc34ae626
Joao Matos <joao@tritao.eu>
2024-05-04 21:50:51 +0100

    Remove WebGPU backend.

--- src/renderer/Renderers.ts
+++ src/renderer/Renderers.ts
@@ -1,18 +1,14 @@
 import { MapViewer } from "../mapviewer/MapViewer";
 import { MapViewerRenderer } from "../mapviewer/MapViewerRenderer";
 import { WebGLRenderer } from "./webgl/WebGLRenderer";
-import { WebGPURenderer } from "./webgpu/WebGPURenderer";
 
-export type RendererType = "webgl" | "webgpu";
+export type RendererType = "webgl";
 export const WEBGL: RendererType = "webgl";
-export const WEBGPU: RendererType = "webgpu";
 
 export function getRendererName(type: RendererType): string {
     switch (type) {
         case WEBGL:
             return "WebGL";
-        case WEBGPU:
-            return "WebGPU";
         default:
             throw new Error("Unknown renderer type");
     }
@@ -25,8 +21,6 @@ export function createRenderer(
     switch (type) {
         case WEBGL:
             return new WebGLRenderer(mapViewer);
-        case WEBGPU:
-            return new WebGPURenderer(mapViewer);
         default:
             throw new Error("Unknown renderer type");
     }
@@ -37,10 +31,6 @@ export function getAvailableRenderers(): RendererType[] {
 
     if (WebGLRenderer.isSupported()) {
         renderers.push(WEBGL);
-    }
-
-    if (WebGPURenderer.isSupported()) {
-        renderers.push(WEBGPU);
     }
 
     return renderers;
